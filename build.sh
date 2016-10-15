205ebb40a419a80fb290a5fc7f47f2f4b7006773
Franco Sanllehi <fsanllehi@gamerlive.cl>
2016-10-15 15:49:45 -0300

    Builder + SPDY

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Compilador Nginx con RTMP
 # https://github.com/DmACKGL/streng
+
+# Colores
 green=`tput setaf 2`
+reset=`tput sgr0`
+
 # Configure
 ./configure \
 	--prefix=/usr \
@@ -13,17 +17,17 @@ green=`tput setaf 2`
 	--with-http_ssl_module \
 	--user=www-data \
 	--group=www-data \
-	--with-http_spdy_module \
-    --with-http_stub_status_module \
+	--with-http_v2_module \
+	--with-http_stub_status_module \
 	--with-http_gzip_static_module \
 	--without-mail_pop3_module \
 	--without-mail_imap_module \
 	--without-mail_smtp_module \
-    --add-module=nginx-rtmp-module \
-    --with-http_stub_status_module
+	--add-module=nginx-rtmp-module \
+	--with-http_stub_status_module
 
 # Make
 make
 make install
 
-echo "${green}NGinx Instalado!"
+echo "${green}NGinx Instalado!${reset}"
